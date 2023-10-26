fd5ece9a106bf87f7b96ef9a42c10332ebd5df3c
Anupam Pandey <20bit071@ietdavv.edu.in>
2023-10-26 11:54:07 +0530

    first commit project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Anupam Pandey",
+    author_email="panupam068@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
+
